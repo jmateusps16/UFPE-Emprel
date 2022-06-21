db5282d1975bb8c891010157cad6c90080e2f272
jmateusps16 <jmateusps16@gmail.com>
2022-06-21 21:27:26 +0000

    doc: registrando proximos passos

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -10,10 +10,12 @@ class ComplaintController extends Controller
     //
 
     public function list() {
+        // TODO: ADICIONAR FILTRO NA BUSCA
         return DefaultResponse(200, 'success', ComplaintModel::all());
     } 
 
     public function save(Request $request) {
+        // TODO: APÓS COLOCAR JWT ADICIONAR USUÁRIO  E FAZER VALIDAÇÃO DE INPUT
         if ($request->id > 0) {
             $model = ComplaintModel::find($request->id);
             
@@ -31,6 +33,7 @@ class ComplaintController extends Controller
     }
 
     public function delete(Request $request) {
+        // TODO: ADICIONAR VALIDAÇÃO
         $model = NULL;
 
         if ($request->id > 0) {
